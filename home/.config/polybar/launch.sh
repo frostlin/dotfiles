f5d03db20f7c7281c82ccd74c51bcf0d6ff4c0e3
frostlin <pavel.toni@yandex.ru>
2022-01-07 16:09:21 +0300

    reworked polybar, based on ArchCraft's polybar configs

--- home/.config/polybar/launch.sh
+++ home/.config/polybar/launch.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env sh
 
-## Add this to your wm startup file.
-
 # Terminate already running bar instances
 killall -q polybar
 
@@ -9,4 +7,4 @@ killall -q polybar
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 # Launch fullbar
-polybar fullbar &
+polybar main &
