2dc021853e5c9dae701c50a0a2b52b91fb2ced80
frostlin <pavel.toni@yandex.ru>
2022-01-09 22:31:27 +0300

    added xsettingsd config and updated install script

--- test_installscipt.sh
+++ test_installscipt.sh
@@ -1,11 +1,9 @@
+#!/bin/bash
 # update system
 sudo pacman -Syu
 
 # install general utilities
-yes | sudo pacman -S curl wget openssh git vim ranger htop alacritty zsh ueberzug telegram-desktop qbittorrent feh noto-fonts-cjk noto-fonts-emoji noto-fonts nmap mpd mpc jp2a flameshot ffmpeg atool unrar unzip p7zip highlight ffmpegthumbnailer fzf exa
-
-# install xorg and WM-related packages
-yes | sudo pacman -S xorg xorg-xinit bspwm xshkd rofi dunst
+sudo pacman -S xorg xorg-xinit bspwm xshkd rofi dunst curl wget openssh openvpn git vim ranger htop alacritty zsh ueberzug telegram-desktop qbittorrent feh noto-fonts-cjk noto-fonts-emoji noto-fonts nmap mpv jp2a flameshot ffmpeg atool unrar unzip p7zip highlight ffmpegthumbnailer fzf exa f2fs-tools exfatprogs dosfstools arc-icon-theme xsettingsd
 
 # install aur helper
 git pull https://aur.arhclinux.org/paru.git
@@ -13,9 +11,11 @@ cd paru
 makepgk -srci paru
 
 # install aur packages
-paru -S polybar vk-messenger brave oh-my-zsh-git pfetch dragon-drag-and-drop ttf-freefont ttf-ms-fonts ttf-linux-libertine ttf-dejavu ttf-inconsolata ttf-ubuntu-font-family discord-canary
+paru -S polybar vk-messenger brave-bin oh-my-zsh-git pfetch dragon-drag-and-drop ttf-freefont ttf-ms-fonts ttf-linux-libertine ttf-dejavu ttf-inconsolata ttf-ubuntu-font-family discord-canary obs-studio-tytan652 fluent-cursor-theme-git zafiro-icon-theme
+
 # change default shell to zsh
 chsh -s /usr/bin/zsh
+
 # install zsh plugins and themes
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
@@ -25,8 +25,7 @@ ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/the
 # install ranger plugins
 cd ~/.config/ranger/plugins
 git clone https://github.com/maximtrp/ranger-archives.git
-
-
-# set up my config files
-
+#https://github.com/IBM/plex
+git clone --depth=1 https://github.com/amix/vimrc.git ~/.vim_runtime
+sh ~/.vim_runtime/install_awesome_vimrc.sh
 
