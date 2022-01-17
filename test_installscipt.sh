eeb8143513b6f97db2fcee0139191d5d66560a1a
frostlin <pavel.toni@yandex.ru>
2022-01-17 20:36:31 +0300

    fixed a lot of mistakes in script

--- test_installscipt.sh
+++ test_installscipt.sh
@@ -3,15 +3,15 @@
 sudo pacman -Syu
 
 # install general utilities
-sudo pacman -S xorg xorg-xinit bspwm xshkd rofi dunst curl wget openssh openvpn git vim ranger htop alacritty zsh ueberzug telegram-desktop qbittorrent feh noto-fonts-cjk noto-fonts-emoji noto-fonts nmap mpv jp2a flameshot ffmpeg atool unrar unzip p7zip highlight ffmpegthumbnailer fzf exa f2fs-tools exfatprogs dosfstools arc-icon-theme xsettingsd
+sudo pacman -S --needed base-devel xorg xorg-apps xorg-xinit bspwm sxhkd rofi dunst curl wget openssh openvpn git vim ranger htop alacritty zsh ueberzug telegram-desktop qbittorrent feh noto-fonts-cjk noto-fonts-emoji noto-fonts nmap mpv jp2a flameshot ffmpeg atool unrar unzip p7zip highlight ffmpegthumbnailer fzf exa f2fs-tools exfatprogs dosfstools arc-icon-theme xsettingsd man terminus-font light
 
 # install aur helper
-git pull https://aur.arhclinux.org/paru.git
+git clone https://aur.archlinux.org/paru.git
 cd paru
-makepgk -srci paru
+makepkg -srci paru
 
 # install aur packages
-paru -S polybar vk-messenger brave-bin oh-my-zsh-git pfetch dragon-drag-and-drop ttf-freefont ttf-ms-fonts ttf-linux-libertine ttf-dejavu ttf-inconsolata ttf-ubuntu-font-family discord-canary obs-studio-tytan652 fluent-cursor-theme-git zafiro-icon-theme
+paru -S --needed polybar vk-messenger brave-bin pfetch dragon-drag-and-drop ttf-freefont ttf-ms-fonts ttf-linux-libertine ttf-dejavu ttf-inconsolata ttf-ubuntu-font-family discord-canary obs-studio-tytan652 fluent-cursor-theme-git zafiro-icon-theme
 
 # change default shell to zsh
 chsh -s /usr/bin/zsh
@@ -25,6 +25,7 @@ ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/the
 # install ranger plugins
 cd ~/.config/ranger/plugins
 git clone https://github.com/maximtrp/ranger-archives.git
+cd ~
 #https://github.com/IBM/plex
 git clone --depth=1 https://github.com/amix/vimrc.git ~/.vim_runtime
 sh ~/.vim_runtime/install_awesome_vimrc.sh
